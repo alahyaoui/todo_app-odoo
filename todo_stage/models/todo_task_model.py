343ff9618c861aa318569c14a6c3045c6a2cc3ab
54895 <54895@etu.he2b.be>
2021-11-25 14:36:06 +0100

    End of Compléter le modèle Task

--- todo_stage/models/todo_task_model.py
+++ todo_stage/models/todo_task_model.py
@@ -5,4 +5,9 @@ class TodoTask(models.Model):
     _inherit = 'todo.task'
 
     effort_estimate = fields.Integer(string="Time Estimation")
+    desc = fields.Char(string="Brief Description")
+    state = fields.Selection([('draft', 'Not Ready'),('open', 'Ready'),('done', 'Ended')], string="State", default='draft')
+    docs = fields.Html(string="Detailed Documentation")
+    date_created = fields.Datetime(string="Creation Date and Time", default=lambda self: fields.datetime.now())
+    image = fields.Binary('Task Image')
     tag_ids = fields.Many2many('todo.task.tag', string="Tags")
