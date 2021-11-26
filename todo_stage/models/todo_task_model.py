699780dca86a652d57625e361334b5c44cf8612b
54895 <54895@etu.he2b.be>
2021-11-26 16:15:47 +0100

    End of Observateur-Observé

--- todo_stage/models/todo_task_model.py
+++ todo_stage/models/todo_task_model.py
@@ -1,12 +1,14 @@
 from odoo import models, fields, api, exceptions
 import logging
 
+
 class TodoTask(models.Model):
     _inherit = 'todo.task'
 
     effort_estimate = fields.Integer(string="Time Estimation")
     desc = fields.Char(string="Brief Description")
-    state = fields.Selection([('draft', 'Not Ready'),('open', 'Ready'),('done', 'Ended')], string="State", default='draft')
+    state = fields.Selection([('draft', 'Not Ready'), ('open', 'Ready'), ('done', 'Ended')], string="State",
+                             default='draft')
     docs = fields.Html(string="Detailed Documentation")
     date_created = fields.Datetime(string="Creation Date and Time", default=lambda self: fields.datetime.now())
     image = fields.Binary('Task Image')
@@ -20,3 +22,14 @@ class TodoTask(models.Model):
 
     def do_reset(self):
         self.state = 'draft'
+
+    @api.onchange('user_id')
+    def _on_change_responsible(self):
+        self.team_ids = None
+        return {
+            'warning': {
+                'title': 'Responsible User Reset',
+                'message': 'Please choose a new Team.',
+                'type': 'notification'
+            }
+        }
